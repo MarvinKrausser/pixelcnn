6bc2c1cea91183fd4f879b497430a42d82884772
Marvin Kraußer <koro.no.omo@gmail.com>
2025-06-12 10:59:32 +0200

    more digits

--- scripts/gen_mnist.py
+++ scripts/gen_mnist.py
@@ -68,8 +68,8 @@ optimizer    = torch.optim.Adam(model.parameters(), lr=1e-3)
 loss_module = nn.CrossEntropyLoss()
 
 
-#show_imgs(sample(model, [24, 1, 28, 28], device, mode_name=os.path.join("gen_mnist", "v20_gen_mnist.tar"), SAVE_PATH=SAVE_PATH))
-#exit()
+show_imgs(sample(model, [24, 1, 28, 28], device, mode_name=os.path.join("gen_mnist", "v55_gen_mnist.tar"), SAVE_PATH=SAVE_PATH))
+exit()
 
 trainPixelCNNGray(model=model, loss_module=loss_module, optimizer=optimizer, train_data_loader=train_loader, test_data_loader=test_loader, 
               validation_data_loader=val_loader, device=device, SAVE_PATH=SAVE_PATH, num_epochs=100, model_name="gen_mnist.tar", folder_name="gen_mnist", train=True)
